583842eb95085cb3eb306394c7571bed35bbb0dd
edoardovicoli <vicoliedoardo@gmail.com>
2016-06-01 12:52:56 +0200

    commit 13: entity per classe Medico

---
+++ src/it/uniroma3/progetto/model/Medico.java
@@ -0,0 +1,71 @@
+package it.uniroma3.progetto.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Medico {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String cognome;
+	
+	@Column(nullable = false)
+	private String specializzazione;
+	
+	public Medico() {}
+
+	public Medico(String nome, String cognome, String specializzazione) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.specializzazione = specializzazione;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getSpecializzazione() {
+		return specializzazione;
+	}
+
+	public void setSpecializzazione(String specializzazione) {
+		this.specializzazione = specializzazione;
+	}
+
+	@Override
+	public String toString() {
+		return "Medico [id=" + id + ", nome=" + nome + ", cognome=" + cognome + ", specializzazione=" + specializzazione
+				+ "]";
+	}
+}
